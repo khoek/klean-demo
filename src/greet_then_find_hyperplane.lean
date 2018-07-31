2e82ef0365226b683dd7ad65a8dbed1de5507910
Keeley Hoek <keeley@hoek.io>
2018-08-01 01:08:37 +1000

    Latest version of the mini find_separating_hyperplane() test

--- src/greet_then_find_hyperplane.lean
+++ src/greet_then_find_hyperplane.lean
@@ -1,14 +1,13 @@
 import system.extras
 import .lib
 
-open extras
-
 def dim : ℕ := 2
-def vects : list (array dim ℕ) := [to_array [1, 2], to_array [4, 5]]
+def a_vects : list (array dim ℕ) := [to_array [1, 2], to_array [4, 5]]
+def b_vects : list (array dim ℕ) := [to_array [5, 5], to_array [7, 8]]
 
 meta def main : io unit := do
     io.print_ln "a",
     extras.greet,
-    n ← pure (extras.find_separating_hyperplane vects),
+    n ← pure (extras.find_separating_hyperplane a_vects b_vects),
     io.print_ln "b",
     io.print_ln n
