abe3f931e1309a997f576bf47d69d458bc2195c8
Keeley Hoek <keeley@hoek.io>
2018-08-03 12:35:50 +1000

    Update demo file

--- src/greet_then_find_hyperplane.lean
+++ src/greet_then_find_hyperplane.lean
@@ -3,11 +3,12 @@ import .lib
 
 def dim : ℕ := 2
 def a_vects : list (array dim ℕ) := [to_array [1, 2], to_array [4, 5]]
-def b_vects : list (array dim ℕ) := [to_array [5, 5], to_array [7, 8]]
+def b_vects : list (array dim ℕ) := [to_array [5, 5], to_array [2, 8]]
 
 meta def main : io unit := do
     io.print_ln "a",
     extras.greet,
     n ← pure (extras.find_separating_hyperplane a_vects b_vects),
     io.print_ln "b",
-    io.print_ln n
+    io.print_ln (array.to_list n.1),
+    io.print_ln n.2
